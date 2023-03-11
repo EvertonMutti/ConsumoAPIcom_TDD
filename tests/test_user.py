d4b2f7a161e242dd25d3cb114c0bcfa8f81cbb30
Everton Mutti <evertoncastro19@gmail.com>
2023-03-11 18:59:48 -0300

    testes completos da tipagem dos atributos, e de dois métodos, obterDados e obterRepositórios

---
+++ tests/test_user.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 14:55:15 2023
+
+@author: Everton
+"""
+try:
+    import sys
+    import os
+
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../api_user'
+            )
+        )
+    )
+except:
+    raise
+    
+import unittest
+from user.user import User
+from unittest.mock import patch
+
+class TestUser(unittest.TestCase):
+    
+    def setUp(self):
+        self.user = User('EvertonMutti')
+    
+    def test_User_assertion_username_str(self):
+        self.assertIsInstance(self.user.username, str)
+    
+    #def test_User_assertion_username_caso_nao_seja_str(self):
+        #with self.assertRaises(AssertionError): 
+            #assert isinstance(self.user.username, (dict,list,tuple,
+                                                   #set,int,float))
+        
+    def test_User_assertion_atributos(self):
+        self.assertIsInstance(self.user.nome, str)
+        self.assertIsInstance(self.user.url_perfil, str)
+        self.assertIsInstance(self.user.num_repos_publicos, int)
+        self.assertIsInstance(self.user.num_seguidores, int)
+        self.assertIsInstance(self.user.num_seguindo, int)
+    
+    def test_obter_dados(self):
+        #Dados falsos
+        resposta_dados_mock = {
+            'name': 'Everton Mutti',
+            'html_url': 'https://github.com/EvertonMutti',
+            'public_repos': 10,
+            'followers': 2,
+            'following': 5,
+        }
+        
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.json.return_value = resposta_dados_mock
+            self.user.obterDados()
+            
+        self.assertEqual(self.user.nome, 'Everton Mutti')
+        self.assertEqual(self.user.url_perfil, 'https://github.com/EvertonMutti')
+        self.assertEqual(self.user.num_repos_publicos, 10)
+        self.assertEqual(self.user.num_seguidores, 2)
+        self.assertEqual(self.user.num_seguindo, 5)
+    
+    def test_repositorios(self):
+        #Dados falsos
+        resposta_mock = [
+            {'name': 'igottafeeling', 'html_url': 'https://github.com/testuser/igottafeeling'},
+            {'name': 'millaeumanoite', 'html_url': 'https://github.com/testuser/millaeumanoite'},
+            {'name': 'prefixodeverao', 'html_url': 'https://github.com/testuser/prefixodeverao'},
+        ]
+        
+        with patch('requests.get') as mock_get:
+            mock_get.return_value.status_code = 200
+            mock_get.return_value.json.return_value = resposta_mock
+            repositorios = self.user.obterRepositorios()
+            
+        for res_mock in resposta_mock:
+            with self.subTest(saida=res_mock.get('html_url')):
+                self.assertEqual(repositorios['igottafeeling'], 'https://github.com/testuser/igottafeeling')
+                self.assertEqual(repositorios['millaeumanoite'], 'https://github.com/testuser/millaeumanoite')
+                self.assertEqual(repositorios['prefixodeverao'], 'https://github.com/testuser/prefixodeverao')
+    
+    def test_gerar_arquivo(self):
+        resposta_dados_mock = {
+            'name': 'Everton Mutti',
+            'html_url': 'https://github.com/EvertonMutti',
+            'public_repos': 10,
+            'followers': 2,
+            'following': 5,
+        }
+        respositorios_mock = [
+            {'name': 'igottafeeling', 'html_url': 'https://github.com/testuser/igottafeeling'},
+            {'name': 'millaeumanoite', 'html_url': 'https://github.com/testuser/millaeumanoite'},
+            {'name': 'prefixodeverao', 'html_url': 'https://github.com/testuser/prefixodeverao'},
+        ]
+    
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+    
+    
+        
